933f1426395c4091975ef2ead7c131060c21765f
meT1moffey <96593529+meT1moffey@users.noreply.github.com>
2025-04-28 18:44:03 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -38,7 +38,7 @@ function upgrade() {
             fruit.elem.remove()
         }
 
-        fruit.elem.style.left = game_window.offsetLeft + fruit.x_pos * game_window.offsetWidth + "px"
+        fruit.elem.style.left = game_window.offsetLeft + fruit.x_pos * game_window.offsetWidth  + "px"
         fruit.elem.style.top  = game_window.offsetTop  + fruit.y_pos * game_window.offsetHeight + "px"
     }
     fruits = remain
