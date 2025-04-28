50f79bf95a36771feed774ee9fd26781dccb801a
meT1moffey <96593529+meT1moffey@users.noreply.github.com>
2025-04-28 19:34:00 +0300

    faster

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ function spawn() {
     let spawned = {}
     spawned.x_pos = Math.random()
     spawned.y_pos = 1.05
-    spawned.x_vel = 0.001 * (0.5 - spawned.x_pos)
-    spawned.y_vel = -0.001
+    spawned.x_vel = 0.01 * (0.5 - spawned.x_pos)
+    spawned.y_vel = -0.01
 
     let elem = document.createElement("span")
     elem.innerHTML = "\ud83c\udf4b" // lemon
