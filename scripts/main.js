0955283c3661d82d75f4295853ba017d4fc6237e
meT1moffey <96593529+meT1moffey@users.noreply.github.com>
2025-04-28 19:12:43 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ function spawn() {
     let spawned = {}
     spawned.x_pos = Math.random()
     spawned.y_pos = 1.1
-    spawned.x_vel = 10 * (0.5 - spawned.x_pos)
-    spawned.y_vel = -10
+    spawned.x_vel = (0.5 - spawned.x_pos)
+    spawned.y_vel = -1
 
     let elem = document.createElement("span")
     elem.innerHTML = "\ud83c\udf4b" // lemon
@@ -18,7 +18,7 @@ function spawn() {
     fruits.push(spawned)
 }
 
-let frameDelay = 1000
+let frameDelay = 10
 
 function upgrade() {
     if(fruits.length == 0) {
@@ -28,9 +28,9 @@ function upgrade() {
     let remain = []
 
     for(let fruit of fruits) {
-        fruit.x_pos += fruit.x_vel / frameDelay
-        fruit.y_pos += fruit.y_vel / frameDelay
-        fruit.y_vel += 0.1 / frameDelay
+        fruit.x_pos += fruit.x_vel * frameDelay
+        fruit.y_pos += fruit.y_vel * frameDelay
+        fruit.y_vel += 0.001 * frameDelay
 
         if(fruit.y_pos < 1 || fruit.y_vel < 0) {
             remain.push(fruit)
@@ -43,7 +43,6 @@ function upgrade() {
         fruit.elem.style.left = game_window.offsetLeft + fruit.x_pos * game_window.offsetWidth  + "px"
         fruit.elem.style.top  = game_window.offsetTop  + fruit.y_pos * game_window.offsetHeight + "px"
         console.log(fruit)
-        /////////
     }
     fruits = remain
 
